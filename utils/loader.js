6494eaa8a5b30e34247c0f4231b38dcf9615f0d2
Geldverkaeufer <stefanhelgert1@gmail.com>
2025-03-24 15:49:55 +0100

    Delete utils directory

--- utils/loader.js
+++
@@ -1,7 +0,0 @@
-export const load = {
-    assets: () => {
-        loadSprite("bgpolizei","./assets/polizeiraum.jpg")
-        loadSprite("bgRaum1","./assets/raum1.jpg")
-        loadSprite("Sprechblase","./assets/Sprechblase.png")
-    }
-}
