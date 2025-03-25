5c757c845ecdd64d34d1a38877a0fc00f8702739
Geldverkaeufer <stefanhelgert1@gmail.com>
2025-03-25 19:33:20 +0100

    Update loader.js

--- utils/loader.js
+++ utils/loader.js
@@ -1,6 +1,6 @@
 export const load = {
     assets: () => {
-        loadSprite("bgpolizei","./assets/polizeiraumSK.jpg")
+        loadSprite("bgpolizei","./assets/polizeiraumSK.png")
         loadSprite("bgRaum1","./assets/raum1.jpg")
         loadSprite("Sprechblase","./assets/Sprechblase.png")
         loadSprite("bgRaum3","./assets/raum3.jpg")
