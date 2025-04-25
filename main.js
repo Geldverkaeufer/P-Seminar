9a4a0fcaf26f2981ed7aeaa2df970525cdc028f8
Geldverkaeufer <stefanhelgert1@gmail.com>
2025-04-25 17:32:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import kaboom from "./libs/kaboom.mjs"
+import { INTRO } from "./utils/Intro.js";
 import { uiManager } from "./utils/UIManager.js";
 import { load } from "./utils/loader.js";
 
@@ -7,7 +8,7 @@ kaboom({
     height: window.innerHeight,
     stretch: true,
     letterbox: true,
-    background: [0, 0, 0]
+    background: [0, 0, 0],
 })
 
 //debug.inspect = true
@@ -16,53 +17,57 @@ load.assets()
 
 
 const scenes = {
-    polizeiRevier: () => {
-        uiManager.displayPolizeiRevier()
-    },
-
-    polizeiMap: () => {
-        uiManager.displayPolizeiMap()
+    intro: () => {
+        INTRO.Zeitung()
     },
 
     raum1: () => {
         uiManager.displayRaum1();
         wait(0.5, () => {uiManager.displayKollegenNachricht(true, "Willkommen im ersten Raum! Ich bin Herr Polizist.\nIch bin immer da, falls du einen Tipp brauchst,\nklick mich einfach an!")})
         let nachricht = 0
-        // Erster Klick
         onClick("Polmann", () => {
             if (nachricht === 0) {
                 destroyAll("Sprechblase");
                 destroyAll("nachricht");
                 destroyAll("Polmann");
+        
                 nachricht += 1;
-    
+        
                 wait(0.01, () => {
                     uiManager.displayKollegenNachricht(true, "Hörst du das? Ich glaube die Luft wird herausgezogen!\nWir müssen schnell eine Sauerstoffzufuhr finden bevor uns die Luft ausgeht...");
                 });
-            }
-        });
-        // Zweiter Klick
-        onClick("Polmann", () => {
-            if (nachricht === 1) {
+        
+            } else if (nachricht === 1) {
                 destroyAll("Sprechblase");
                 destroyAll("nachricht");
                 destroyAll("Polmann");
-    
-                // Jetzt den Rest ausführen
-                wait(0.01,()=>{uiManager.displayKollegenNachricht(false,"laweiuf");uiManager.areaGasflaschen()})
+        
+                wait(0.01, () => {
+                    uiManager.displayKollegenNachricht(false, "laweiuf");
+                    uiManager.areaGasflaschen();
+                });
+        
                 uiManager.vergroesserPolmann("Wir sind hier in einem Chemie-Labor!\nBestimmt finden wir irgendwo Gasflaschen...");
-                uiManager.displayGlühbirne()
-                uiManager.verkleinerPolmann();;
-                onClick("kreis", () => { go("r1t1"); });
-    
-                nachricht += 1; // Falls du noch weitere Schritte hinzufügen willst
+                uiManager.displayGlühbirne();
+                uiManager.verkleinerPolmann();
+        
+                onClick("kreis", () => {
+                    go("r1t1");
+                });
+        
+                nachricht += 1;
             }
         });
+        
     },
 
     r1t1: () => {
         uiManager.raum1t1()
         onKeyPress("space",() => {go("raum3")})
+    },
+
+    r1t3: () => {
+
     },
 
     raum2: () => {},
@@ -88,5 +93,5 @@ for (const key in scenes) {
     scene(key,scenes[key])
 }
 
-go("polizeiRevier")
+go("intro")
 console.log(height(),width())
