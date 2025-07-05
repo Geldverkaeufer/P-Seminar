4510fa664d67999e6ec34ce564b92d015dd7e7b5
Geldverkaeufer <stefanhelgert1@gmail.com>
2025-07-05 16:57:24 +0200

    Update Intro.js

--- utils/Intro.js
+++ utils/Intro.js
@@ -1,12 +1,15 @@
 import { uiManager } from "./UIManager.js";
 
+//fertige PolizeiMap hochladen
+// anfangsscreen mit reload erklären und so
+
 class Intro {
 
     Zeitung() {
         add([
             sprite("Zeitung"),
             area(),
-            scale(width() /2048, height() /1152),
+            scale(1),
             pos(width() / 2, height() / 2),
             anchor("center"),
             fixed(),
@@ -50,24 +53,53 @@ class Intro {
         add([
             sprite("bgpolizei"),
             area(),
-            scale(width() / 1666, height() / 907),
+            scale(width() / 1492, height() / 811),
             pos(width() / 2, height() / 2),
             anchor("center"),
             fixed(),
             "PolizeiBG"
         ]);
+         
+        wait(1.3,()=>{
+            add([
+                sprite("kreis"),
+                area(),
+                pos(width()/10*5.85,height()/10*7.1),
+                anchor("center"),
+                scale(width()/1200,height()/1150),
+                opacity(0),
+                "zu1"
+            ])
+            add([
+                sprite("kreis"),
+                area(),
+                pos(width()/10*5.3,height()/10*4.6),
+                anchor("center"),
+                scale(width()/1500,height()/650),
+                opacity(0),
+                "zu2"
+            ])
+            add([
+                sprite("kreis"),
+                area(),
+                pos(width()/10*4.16,height()/10*4.57),
+                anchor("center"),
+                scale(width()/2200,height()/630),
+                opacity(0),
+                "zu3"
+            ])
+        })
+        onClick("zu1",()=>{go("raum1")})
+        onClick("zu2",()=>{uiManager.displayKollegenNachricht(true,"Der zweite Raum ist leider noch versperrt.")})
+        onClick("zu3",()=>{uiManager.displayKollegenNachricht(true,"Der dritte Raum ist leider noch versperrt.")})
 
-        this.weiter(0.5, () => {
-            destroyAll("*");
-            go("raum1")
-        });
         this.zurück(0.5, () => {
             destroyAll("*");
             wait(0.01, () => {
                 INTRO.PolizeiRevier();
             });
         });
-        wait(1,()=>{uiManager.displayKollegenNachricht(true,"Hier ist die Übersicht zu unserem Fall.\nDu kannst sie dir jederzeit anschauen!");uiManager.verkleinerPolmann();uiManager.vergroesserPolmann2()})
+        uiManager.displayKollegenNachricht(true,"Hier ist die Übersicht zu unserem Fall.\nLass uns zuerst in den ersten Raum gehen!");uiManager.verkleinerPolmann();uiManager.vergroesserPolmann2()
 
     }
 
